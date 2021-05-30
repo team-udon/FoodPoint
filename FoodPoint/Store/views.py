f6dbf0a84019a2cd34e8b734993f1a58d49b4d03
System종료 <wsbs9805@naver.com>
2021-05-30 18:15:01 +0900

    Store-register
    가게 등록, 하지만 미완인... 진짜 글작성만 됩니당

--- FoodPoint/Store/views.py
+++ FoodPoint/Store/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def new(request):
-    return render(request, 'tempNew.html')
+    return render(request, 'RegisterStore/registerStore-signUp.html')
 
 def create(request): 
     new_Store=Store()
@@ -14,7 +14,6 @@ def create(request):
     new_Store.Store_tel=request.POST['Store_tel']
     new_Store.Store_add=request.POST['Store_add']
     new_Store.Store_num=request.POST['Store_num']
-
     new_Store.author=request.user
     new_Store.save()
     return redirect('home')
