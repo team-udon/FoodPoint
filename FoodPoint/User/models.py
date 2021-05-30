229791dbd1c0de840b7eaf1947fdb83a6cb706fc
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2021-05-30 19:34:41 +0900

    user

---
+++ FoodPoint/User/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    photo = models.ImageField(upload_to = "User/",blank = True, null = True)
+    user = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField()
+
+
---
+++ FoodPoint/User/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+# Create your views here.
+def photoUpload(request):
+    new_post = Post()
+    new_post.photo =  request.FILES['image']
+    new_post.user = request.POST['user']
+    new_post.pub_date = timezone.now()
+
+    new_post.save()
+ 
